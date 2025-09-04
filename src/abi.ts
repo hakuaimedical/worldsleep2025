ee8be01094f0b2af68844c546e2a2fc998ae8e0a
hakuaimedical <hakuai.medical@gmail.com>
2025-09-04 18:08:18 +0900

    Update abi.ts

--- src/abi.ts
+++ src/abi.ts
@@ -1,1 +1,12 @@
-// abi.ts content
+export const DataRegistryAbi = [
+  {
+    "type":"event","name":"DataPublished","inputs":[
+      {"name":"owner","type":"address","indexed":true},
+      {"name":"cid","type":"string","indexed":false}
+    ]
+  },
+  {
+    "type":"function","name":"publish","stateMutability":"nonpayable",
+    "inputs":[{"name":"cid","type":"string"}],"outputs":[]
+  }
+] as const;
